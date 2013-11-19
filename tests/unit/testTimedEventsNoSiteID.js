57a3ee044bd19f63808b88dcf9a52bcecede1249
Richard Robinson <richard.robinson@forrent.com>
2013-11-19 15:58:35 -0500

    fixed test

--- tests/unit/testTimedEventsNoSiteID.js
+++ tests/unit/testTimedEventsNoSiteID.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 exports.testTimedEventsNoSiteID = function(test) {
 	var url = "http://localhost:4000/log-event?code=Community_Theater_High_Bandwidth_Time_Viewed&time=39&length=96&sessionid=iu5l14qc9lojfateajfgga3ic4&eventtime=1366237451&jsguid=73e26b2a-6adf-4625-A4bb-db2073a085d4";
-	var req = http.request(url, function(res) 
+	var req = http.request(url, function(res) {
       res.on('data', function(d) {
         process.stdout.write(d);
       });
