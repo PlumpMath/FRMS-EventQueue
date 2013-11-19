e8a8c95da814259c1ad7dfd7fd9fe7aff6dd7483
Scott Minter <scott.minter@DFRFRM027446.local>
2013-11-19 09:28:21 -0500

    added comment to sanity check

--- tests/unit/test.js
+++ tests/unit/test.js
@@ -1,6 +1,8 @@
 var httputil = require('nodeunit').utils.httputil;
 
+//	Sanity check to verify nodeunit working
 exports.testRouteToListingsController = function(test) {
         test.ok(true, 'everything is ok');
         test.done();
 };
+
